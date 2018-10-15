2ffb4ea773388359fd193c8be6acaff2cf7fac59
VictorParodi <victor.parodi.montero@gmail.com>
2018-10-14 20:50:14 -0500

    app1 commit 1: Adding new texts components and styles

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24º</Text>
       </View>
     );
   }
@@ -18,4 +20,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textStyle: {
+    textAlign: 'center',
+    // conditional based on OS
+    // fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto'
+    fontFamily: 'Roboto',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'AvenirNext-Regular'  
+      }
+    })
+  },
+
+  largeText: {
+    fontSize: 44
+  },
+
+  smallText: {
+    fontSize: 18
+  }
 });
