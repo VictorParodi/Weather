6f8c63b8518c4a06536508f88d939a28198d8a00
VictorParodi <victor.parodi.montero@gmail.com>
2018-10-18 00:16:09 -0500

    app2 commit 3: using props and state to use data flow

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -2,16 +2,44 @@ import React from 'react';
 import { View, StyleSheet, TextInput } from 'react-native';
 
 export default class SearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        }
+    }
+
+    handleChangeText = (text) => {
+        this.setState({text});
+    }
+
+    handleSubmitEditing = () => {
+        const {onSubmit} = this.props,
+              {text} = this.state;
+
+        if (!text) return;
+        
+        onSubmit(text);
+        this.setState({ text: '' });
+    }
+
     render() {
+        const {placeholder} = this.props;
+        const {text} = this.state;
+
         return (
             <View style={styles.container}>
                 <TextInput
                     autoCorrect={false}
-                    placeholder={this.props.placeholder}
+                    value={text}
+                    placeholder={placeholder}
                     placeholderTextColor="white"
                     underlineColorAndroid="transparent"
                     style={styles.textInput}
                     clearButtonMode="always"
+                    onChangeText={this.handleChangeText}
+                    onSubmitEditing={this.handleSubmitEditing}
                 />
             </View>
         );
@@ -32,6 +60,6 @@ const styles = StyleSheet.create({
         flex: 1,
         color: 'white',
         width: 300,
-        alignSelf: 'center'
+        paddingHorizontal: 5
     }
 });
